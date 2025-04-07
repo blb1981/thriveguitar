c622b0c691c10aaa42881d3a553b1ada3ff38ac8
Brandon Bland <brandonbland90@gmail.com>
2025-04-06 23:55:15 -0400

    Removed class from article tag

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <main>
   <div>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article class="text-2xl">
+        <article>
           <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <div><?php the_content(); ?></div>
         </article>
